de831f401a6f7c57a79a554fb9e9887940c03d4d
Phillip <philmography2010@gmail.com>
2024-09-05 20:22:33 -0500

    first commit

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -3,11 +3,17 @@ package org.launchcode;
 import java.util.Scanner;
 
 public class Area {
-    public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter a radius: ");
-    double radius = input.nextDouble();
-    double area = Circle.getArea(radius);
-    System.out.print("The area of circle of radius: " + radius + " is " + area);
+    public static void main(String[] args) {
+        boolean isValid = true;
+        double radius = 1;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a radius: ");
+            radius = input.nextDouble();
+            double area = Circle.getArea(radius);
+            System.out.print("The area of circle of radius: " + radius + " is " + area);
+
+        }while(radius < 0);
+
     }
 }
