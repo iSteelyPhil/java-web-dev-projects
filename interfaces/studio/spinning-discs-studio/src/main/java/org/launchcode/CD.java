db0113528edbf480f45bf36940e1bfa48fae43ec
Phillip <philmography2010@gmail.com>
2024-10-03 01:04:04 -0500

    Ch5 Studio

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
@@ -1,8 +1,81 @@
 package org.launchcode;
 
-public class CD {
+public class CD extends Media implements Rewritable {
     // TODO: Implement your custom interface.
+    private boolean isMusicCD;
+
+    public CD(String name, boolean isMusicCD) {
+        super(name, "", 800, 700);
+        this.isMusicCD = isMusicCD;
+        setDiscType(isMusicCD ? "music CD" : "CD-RW");
+    }
+
+    public CD(String name) {
+        this(name, false);
+    }
+
+    public boolean isMusicCD() {
+        return isMusicCD;
+    }
+
+    public void setMusicCD(boolean musicCD) {
+        isMusicCD = musicCD;
+    }
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
+
+
+    @Override
+    public String toString() {
+        String header = isMusicCD ? "Tracks" : "Files";
+        return super.toString() + getFormattedFileList(header);
+    }
+
+    //rewrtiable methods
+
+    @Override
+    public void writeFile(File file) {
+        spinDisc();
+        String fileType = isMusicCD ? "Track" : "File";
+        if (getFiles().contains(file)) {
+            System.out.println("The " + fileType + " " + file.getName() + " has already been added.");
+        } else if (getSpaceUsed() + file.getSize() > getCapacity()) {
+            System.out.println("WARNING: THERE IS NOT ENOUGH SPACE ON THE " + getDiscType() + " for " + file.getName() + ".");
+        } else {
+            getFiles().add(file);
+            System.out.println("The " + fileType + " " + file.getName() + " has been added to " + getName() + ".");
+        }
+    }
+
+    @Override
+    public void removeFile(File file) {
+        if (isMusicCD) {
+            System.out.println("Single files cannot be removed from a music CD once written.");
+        } else {
+            spinDisc();
+            if (fileIsPresent(file)) {
+                getFiles().remove(file);
+                System.out.println("The file " + file.getName() + " has been removed from the " + getDiscType());
+            }
+        }
+    }
+
+    @Override
+    public void reformatDisc() {
+        spinDisc();
+        getFiles().clear();
+        setMusicCD(false);
+        setDiscType("CD-RW");
+        setName("Unnamed CD-RW");
+    }
+
+    @Override
+    public void runFile(File file) {
+        if (fileIsPresent(file)) {
+            spinDisc();
+            String verb = isMusicCD ? "Playing" : "Opening file ";
+            System.out.println(verb + " " + file.getName() + "...");
+        }
+    }
 }
--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,42 @@ public class Main {
 
         // TODO: Declare and initialize a CD and a DVD object.
 
+        CD coheedAlbumOne = new CD("The Second Stage Turbine Blade", true);
+        CD warcraftThree = new CD("Warcraft 3: Reign of Chaos");
+        DVD seinfeldSeason3 = new DVD("Seinfeld: Season 3");
+
+        File timeConsumer = new File("Time Consumer", 50);
+        File everythingEvil = new File("Everything Evil", 60);
+        coheedAlbumOne.writeFile(timeConsumer);
+        coheedAlbumOne.writeFile(everythingEvil);
+
+        File scourgeCampaign = new File("The Scourge Campaign", 100);
+        File orcCampaign = new File("The Orc Campaign", 100);
+        warcraftThree.writeFile(scourgeCampaign);
+        warcraftThree.writeFile(orcCampaign);
+
+        File s3e1 = new File("Season 3 - Episode 1", 310);
+        File s3e2 = new File("Season 3 - Episode 2", 320);
+        seinfeldSeason3.writeFile(s3e1);
+        seinfeldSeason3.writeFile(s3e2);
+
         // TODO: Call each CD and DVD method to verify that they work as expected.
+
+        System.out.println(coheedAlbumOne);
+        System.out.println(warcraftThree);
+        System.out.println(seinfeldSeason3);
+
+        //
+        coheedAlbumOne.runFile(everythingEvil);
+        seinfeldSeason3.writeFile(s3e2);
+        //
+        warcraftThree.removeFile(orcCampaign);
+        warcraftThree.runFile(orcCampaign);
+        //
+        coheedAlbumOne.reformatDisc();
+        coheedAlbumOne.runFile(everythingEvil);
+        //
+        File tooBigFile = new File("TOOOBIIIIIG", 10000);
+        coheedAlbumOne.writeFile(tooBigFile);
     }
 }
